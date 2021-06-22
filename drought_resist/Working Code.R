d7bd94173084c8e754842ff6b7787cce202ac6a4
surlyqaqc <kims@surlybrewing.com>
2021-06-22 10:46:45 -0700

    test

---
+++ drought_resist/Working Code.R
@@ -0,0 +1,23 @@
+if(!requireNamespace("BiocManager")){
+  install.packages("BiocManager")
+}
+BiocManager::install("phyloseq")
+
+library(vegan)
+library(dada2)
+library(Biostrings)
+library(ShortRead)
+library(vegan)
+library(ggplot2)
+library(dplyr)
+library(plyr)
+library(data.table)
+library(ggpubr)
+library(RColorBrewer)
+library(ape)
+library(car)
+library(beepr)
+library(plotly)
+library(phyloseq)
+library(hillR)
+
